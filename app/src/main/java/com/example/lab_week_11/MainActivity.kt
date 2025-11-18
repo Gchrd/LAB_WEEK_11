074d904e23ef24a8bab75f36f934f27251890a5b
Gchrd <richardgiansanto111@gmail.com>
2025-11-18 11:21:36 +0700

    Commit No. 1: Building App with SharedPreferences and DataStore

---
+++ app/src/main/java/com/example/lab_week_11/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.lab_week_11
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the settings store from the application
+        val settingsStore =
+            (application as SettingsApplication).settingsStore
+
+        // Create the view model instance with the settings store as the constructor parameter
+        val settingsViewModel = ViewModelProvider(
+            this,
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    @Suppress("UNCHECKED_CAST")
+                    return SettingsViewModel(settingsStore) as T
+                }
+            }
+        )[SettingsViewModel::class.java]
+
+        // Observe the text live data
+        settingsViewModel.textLiveData.observe(this) { text ->
+            // Update the text view when the text live data changes
+            findViewById<TextView>(R.id.activity_main_text_view).text = text
+        }
+
+        findViewById<Button>(R.id.activity_main_button).setOnClickListener {
+            // Save the text when the button is clicked
+            val input = findViewById<EditText>(R.id.activity_main_edit_text)
+                .text.toString()
+            settingsViewModel.saveText(input)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lab_week_11/SettingsViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.lab_week_11
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class SettingsViewModel(
+    private val settingsStore: SettingsStore
+) : ViewModel() {
+
+    // The text live data is used to notify the activity when the text changes
+    private val _textLiveData = MutableLiveData<String>()
+    val textLiveData: LiveData<String> = _textLiveData
+
+    init {
+        // Launch a coroutine to get the text from the data store asynchronously
+        viewModelScope.launch {
+            settingsStore.text.collect { value ->
+                _textLiveData.postValue(value)
+            }
+        }
+    }
+
+    // Save the text to the data store
+    fun saveText(text: String) {
+        // Launch a coroutine to save the text to the data store asynchronously
+        viewModelScope.launch {
+            settingsStore.saveText(text)
+        }
+    }
+}
